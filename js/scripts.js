c689e8801418af86a456b2692aacb159297cbd81
santio89 <santiolais1989@gmail.com>
2022-01-18 20:59:36 -0300

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -86,7 +86,7 @@ function toggleLista(){
             if (items == bebidas.id){
                 let carrito__newLi = document.createElement("li");
                 carrito__newLi.innerHTML = `
-                <span><span>◖</span>${bebidas.nombre}</span>
+                <span>${bebidas.nombre}</span>
                 <span>${bebidas.contNeto}ml</span>
                 <span>$${bebidas.precio}</span>
                 <span>(x${bebidas.inShopList()})</span>
