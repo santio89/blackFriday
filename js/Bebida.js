eccaf8412080cfed2000e737f47cb14c92df26c2
santio89 <santiolais1989@gmail.com>
2022-01-20 23:05:49 -0300

    js and styles

--- js/Bebida.js
+++ js/Bebida.js
@@ -30,32 +30,5 @@ class Bebida{
     }
 }
 
-class Combo{
-    constructor(productos, descuento, nombre, id){
-        this.productos = productos;
-        this.precioTotal = 0;
-        this.descuento = descuento;
-        this.nombre = nombre;
-        this.id = id; 
-    }
-
-    calcPrecioTotal(){
-        for (let i=0; i<this.productos.length;i++){
-            this.precioTotal += this.productos[i].precio;
-        }
-        this.precioTotal -= Math.round((this.descuento / 100) * this.precioTotal);
-    }
-
-    inShopList(){
-        let i = 0;
-        for (const items of shopCart1.shopList){
-            if (items == this.id){
-                i++;
-            }
-        }
-        
-        return i;
-    }
-}
 
 /* module.exports = Bebida; */
