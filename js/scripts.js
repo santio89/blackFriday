7967d123978e1f26e2f4a33816942d413f3707d0
santio89 <santiolais1989@gmail.com>
2022-01-21 18:12:35 -0300

    js/styles small fix

--- js/scripts.js
+++ js/scripts.js
@@ -405,22 +405,26 @@ for (let i=0; i<stock1.arrayCombosFeatured.length; i++){
     let wrapper__item = document.createElement("div");
     wrapper__item.classList.add("ofertasFeatured__wrapper__item");
 
-    wrapper__item.innerHTML = `
+    let wrapper__item__content = document.createElement("div");
+    wrapper__item__content.classList.add("ofertasFeatured__wrapper__item__content");
+
+    wrapper__item__content.innerHTML = `
     <h3><span>COMBO ${combo.nombre} &#8674; </span>$${combo.precioTotal}</h3>`;
 
     for (let j=0; j<combo.productos.length;j++){
         let combo__item = combo.productos[j];
-        wrapper__item.innerHTML += `
+        wrapper__item__content.innerHTML += `
         <p>${combo__item.tipo} ${combo__item.marca} ${combo__item.contNeto}ml</p>
         `
     }
 
-    wrapper__item.innerHTML += `
-    <div class="ofertasFeatured__wrapper__item__buttonContainer">
+    wrapper__item__content.innerHTML += `
+    <div class="ofertasFeatured__wrapper__item__content__buttonContainer">
     <button class="${combo.nombre}__menos" onclick="shopCart1.removeItem(${combo.id})">-</button>
     <button class="${combo.nombre}__mas" onclick="shopCart1.addItem(${combo.id})">+</button>
     </div>
     `
     
     ofertasFeatured.appendChild(wrapper__item);
+    wrapper__item.appendChild(wrapper__item__content);
 }
