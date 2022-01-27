9e0d9029754734ab289910dc5565282c59cdc7ca
santio89 <santiolais1989@gmail.com>
2022-01-27 17:10:14 -0300

    small fix

--- js/scripts.js
+++ js/scripts.js
@@ -264,7 +264,7 @@ function toggleProductos(){
     }
 
     if (productos.classList.contains("visible")){
-        setTimeout(()=>{productos.classList.remove("visible"); productos.scrollTo(0,0); filter.value = 'todas'; productFilter()}, 200);
+        setTimeout(()=>{productos.classList.remove("visible"); productos.scrollTo(0,0); filter.value = "todas"; productFilter()}, 200);
     } else{
         productos.classList.add("visible");
     }
@@ -272,7 +272,7 @@ function toggleProductos(){
     let ofertas = document.querySelector(".ofertasPage");
     if (ofertas.classList.contains("carrito__lista__translated")){
         ofertas.classList.toggle("carrito__lista__translated");
-        setTimeout(()=>{ofertas.classList.remove("visible"); ofertas.scrollTo(0,0);filter.value = 'todas'; productFilter()}, 200);
+        setTimeout(()=>{ofertas.classList.remove("visible"); ofertas.scrollTo(0,0);filter.value = "todas"; productFilter()}, 200);
     }
 
     /* al abrir la seccion, esconder el overflow del body y compensar con margen por el ancho de la scrollbar (que desaparece) */
